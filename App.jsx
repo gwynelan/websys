ca40af22254a90a9b14424b5a9e495ef0e8b3b94
gwynelan <167177683+gwynelan@users.noreply.github.com>
2024-06-11 01:52:08 +0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,11 +1,22 @@
-import HomePage from "./pages/HomePage";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
 import './css/web.css';
-
-
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import MenuPage from './pages/MenuPage';
 
 function App() {
   return (
-    <HomePage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/elan-lumay" element={<HomePage />} />
+        <Route path="/elan-lumay/about" element={<AboutPage />} />
+        <Route path="/elan-lumay/contact" element={<ContactPage />} />
+        <Route path="/elan-lumay/menu" element={<MenuPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
